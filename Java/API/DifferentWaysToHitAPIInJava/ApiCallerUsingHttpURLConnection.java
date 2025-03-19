08e70d66e9caf2887157e6e14386d43fe878f84e
Subham Singh <subhams@azuga.com>
2025-03-19 12:40:06 +0530

    Updated Todays Learning

--- Java/API/DifferentWaysToHitAPIInJava/ApiCallerUsingHttpURLConnection.java
+++ Java/API/DifferentWaysToHitAPIInJava/ApiCallerUsingHttpURLConnection.java
@@ -1,11 +1,11 @@
-package Java.API;
+package Java.API.DifferentWaysToHitAPIInJava;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class ApiCaller {
+public class ApiCallerUsingHttpURLConnection {
     public static void main(String[] args) {
         try {
             // Sample API endpoint
---
+++ Java/Kafka/KafkaDemo/src/main/java/com/example/KafkaDemp/test.java
@@ -0,0 +1,4 @@
+package com.example.KafkaDemp;
+
+public class test {
+}
