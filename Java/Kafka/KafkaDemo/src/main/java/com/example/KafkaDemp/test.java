42c200d1b9bda009326ba91102e6485bb72ca14e
Subham Singh <subhams@azuga.com>
2025-03-21 11:40:14 +0530

    Added deployement

--- Java/Kafka/KafkaDemo/src/main/java/com/example/KafkaDemp/test.java
+++ Java/Kafka/KafkaDemo/src/main/java/com/example/KafkaDemp/test.java
@@ -1,4 +1,17 @@
 package com.example.KafkaDemp;
 
-public class test {
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class test {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+
+        int[] n = {1 ,2 ,3 ,3 ,4, 5 };
+    }
+
+    public static int majorityElement(int[] nums) {
+        Arrays.stream(nums).collect(Collectors.groupingBy(x->x),Collectors.maximumofthegroup);
+        return 0;
+    }
 }
