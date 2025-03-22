3a86459e0d4b89df35129156595d7ec723abd936
Subham Singh <subhams@azuga.com>
2025-03-22 23:54:32 +0530

    added some spring boot topic

--- WebDevelopment/SpringBoot/Spring-Boot/src/main/java/com/BackendWithJava/Spring/Boot/RequestParamVsRequestBody/RequestParamVsRequestBody.java
+++ WebDevelopment/SpringBoot/Spring-Boot/src/main/java/com/BackendWithJava/Spring/Boot/RequestParamVsRequestBody/RequestParamVsRequestBody.java
@@ -1,10 +1,10 @@
-package com.BackendWithJava.Spring.Boot.RequestParamVsResponseBody;
+package com.BackendWithJava.Spring.Boot.RequestParamVsRequestBody;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RequestBodyVsResponseBody {
+public class RequestParamVsRequestBody {
 
     @GetMapping("/getUsingRequestBody")
     public void getuUsingRequestBody(){
