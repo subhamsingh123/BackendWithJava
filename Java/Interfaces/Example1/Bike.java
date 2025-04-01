2b4f95d6548933b31de097cd46c10212c190a992
Subham Singh <subhams@azuga.com>
2025-04-01 17:45:24 +0530

    Added On Interfaces

---
+++ Java/Interfaces/Example1/Bike.java
@@ -0,0 +1,29 @@
+package Java.Interfaces.Example1;
+
+public class Bike implements Vehicle {
+
+    @Override
+    public int getNumberOfTires() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNumberOfTires'");
+    }
+
+    @Override
+    public String GetType() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'GetType'");
+    }
+
+    @Override
+    public String GetColor() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'GetColor'");
+    }
+
+    @Override
+    public String GetManufacturingDate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'GetManufacturingDate'");
+    }
+    
+}
